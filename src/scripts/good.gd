b54b5a38fae6e8f17d0ad5c14648396e0eae3b09
pabweigar@alum.us.es <pabweigar@alum.us.es>
2025-02-13 15:13:42 +0100

    Primer commit :)

--- src/scripts/good.gd
+++ src/scripts/good.gd
@@ -3,7 +3,10 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var type: int
+	var demand: int
+	var offer: int
+	var price: float
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
